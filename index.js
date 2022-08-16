fbde741b5a461305ffd834383c776235252d0554
Melissa Fields <melissamfields12@gmail.com>
2022-08-16 12:22:26 -0700

    commit tree b50921dca99af7c2e5c8ca5e15c9571642f1b1ad

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const events = "surprise"
+function writeCards(names, events) {
+    let message = []
+for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    message.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+}
+return message;
+}
+writeCards(names, events);
+
+let number = 10;
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
